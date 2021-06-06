09ad2ec4e5cef7a282d644f67a9baa071bd77b51
arek <areksa123@o2.pl>
2021-06-06 22:43:14 +0200

    d

--- src/game/Menu.js
+++ src/game/Menu.js
@@ -10,9 +10,20 @@ export default class Menu {
         this.nodeMenu.onclick = (e) => { e.stopPropagation(); }
         document.body.appendChild(this.nodeMenu);
         this.options = new Array();
+        this.initOptions();
     }
     initOptions = () => {
-
+        this.undo = document.createElement("div")
+        this.undo.classList.add("opt")
+        let text1 = document.createElement("div")
+        text1.innerText = "Undo"
+        text1.style.float = "left"
+        let text2 = document.createElement("div")
+        text2.innerText = "Control + Z"
+        text2.style.float = "right"
+        this.undo.append(text1, text2)
+        this.nodeMenu.appendChild(this.undo)
+        this.undo.onclick = (e) => { e.stopImmediatePropagation; console.log("undo") }
     }
 
 
